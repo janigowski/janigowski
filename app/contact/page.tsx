e4b266b0c9e4b588f5bdbf88de1001dde731ab12
Dawid Janiga <dawidjaniga@gmail.com>
2024-06-26 18:05:24 +0200

    feat: update brand

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/janigowski_",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@janigowski_",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:dev@janigowski.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "dev@janigowski.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/janigowski",
 		label: "Github",
-		handle: "chronark",
+		handle: "janigowski",
 	},
 ];
 
