d12d3ce3d56a138d2132d83aaa44095bfc56212d
Dawid Janiga <dawidjaniga@gmail.com>
2025-01-10 23:09:14 +0100

    feat(nav): polish

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ArrowLeft } from "lucide-react";
+import { Hexagon } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 
@@ -20,13 +20,20 @@ export const Navigation: React.FC = () => {
 	return (
 		<header ref={ref}>
 			<div
-				className={`fixed inset-x-0 top-0 z-50 backdrop-blur  duration-200 border-b  ${isIntersecting
-					? "bg-zinc-900/0 border-transparent"
-					: "bg-zinc-900/500  border-zinc-800 "
+				className={`mx-auto sm:container fixed top-4 inset-x-4 sm:inset-x-0 z-50 rounded-2xl shadow-[inset_0_1px_1px_0_hsla(0,0%,100%,.15)] border border-white/10 backdrop-blur  duration-200  ${isIntersecting
+					? "bg-zinc-800/40 "
+					: "bg-zinc-800/70 "
 					}`}
 			>
-				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
-					<div className="flex justify-between gap-8">
+				<div className="flex items-center justify-between p-6">
+					<Link
+						href="/"
+						className="duration-200 text-zinc-300 hover:text-zinc-100"
+					>
+						<Hexagon className="w-6 h-6 " />
+					</Link>
+
+					<div className="flex justify-around gap-8 text-sm">
 						<Link
 							href="/projects"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
@@ -52,13 +59,6 @@ export const Navigation: React.FC = () => {
 							Contact
 						</Link>
 					</div>
-
-					<Link
-						href="/"
-						className="duration-200 text-zinc-300 hover:text-zinc-100"
-					>
-						<ArrowLeft className="w-6 h-6 " />
-					</Link>
 				</div>
 			</div>
 		</header>
