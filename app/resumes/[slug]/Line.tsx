fdccf4a49db6f815f01a020f9234ac289997e2dd
Dawid Janiga <dawidjaniga@gmail.com>
2025-02-11 11:51:42 +0100

    fix(resume): stack lines in one row

--- app/resumes/[slug]/Line.tsx
+++ app/resumes/[slug]/Line.tsx
@@ -1,8 +1,8 @@
 export default function Line() {
     return (
         <div className="relative h-4">
-            <hr className="border-zinc-800 w-10" />
-            <hr className="border-zinc-300" />
+            <hr className="absolute top-0 left-0 right-0 border-zinc-300" />
+            <hr className="absolute top-0 left-0 right-0 border-zinc-800 w-10" />
         </div>
     )
 }
