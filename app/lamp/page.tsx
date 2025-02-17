e5b7929a803721c99a0998e2db691ed27e3e2896
Dawid Janiga <dawidjaniga@gmail.com>
2025-02-17 20:02:01 +0100

    feat(lamp): init page

---
+++ app/lamp/page.tsx
@@ -0,0 +1,16 @@
+import { RegularHeader } from "../components/regular-header";
+
+export default function Lamp() {
+	return (
+		<>
+			<RegularHeader
+				title="Background Lamp"
+				description="You can control my background lamp here. If you want to see the effect just contact to have a video call."
+			/>
+			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
+
+
+			</div>
+		</>
+	);
+}
