83f4ea7bf43344f1b115340e9979755462bd0932
Dawid Janiga <dawidjaniga@gmail.com>
2025-02-17 20:19:12 +0100

    feat(lamp): allow to control color using picker

--- app/lamp/controls.tsx
+++ app/lamp/controls.tsx
@@ -1,14 +1,23 @@
 "use client";
 
 import { turn, setColor } from "@/services/hue";
+import { useState } from "react";
+import { HexColorPicker } from "react-colorful";
 
 export default function Controls() {
+    const [color, setLocalColor] = useState("#aabbcc");
+
+    function handleColorChange(color: string) {
+        setColor(color);
+    }
+
     return (
         <>
             <button onClick={() => turn('on')}>Turn on</button>
             <button onClick={() => turn('off')}>Turn off</button>
             <button onClick={() => setColor('#acdb00')}>Set color Acidlime</button>
             <button onClick={() => setColor('#0078d4')}>Set color Blue</button>
+            <HexColorPicker color={color} onChange={handleColorChange} />
         </>
 
     );
