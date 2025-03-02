8a2a32c0d2c161bebe37d384a85da0c17569e1a2
Dawid Janiga <dawidjaniga@gmail.com>
2025-03-02 10:26:06 +0100

    refactor(experience): remove page

--- app/experience/layout.tsx
+++
@@ -1,9 +0,0 @@
-import ContentLayout from "../components/content-layout";
-
-export default function ExperienceLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return <ContentLayout>{children}</ContentLayout>;
-} 
