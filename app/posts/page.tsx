1d7c4a04457e3cfa7dc58b64b40530554f4366a1
Dawid Janiga <dawidjaniga@gmail.com>
2025-04-28 23:50:32 +0200

    feat(posts): list book reviews

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { allPosts } from "contentlayer/generated";
+import { allPosts, allBooks } from "contentlayer/generated";
 import { Card } from "../components/card";
 import { Article } from "./article";
+import BookArticle from "../library/article";
 import { Metadata } from "next";
 import { RegularHeader } from "../components/regular-header";
 import StaggeredAnimation from "../components/StaggeredAnimation";
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
 
 export const revalidate = 60;
 export default async function postsPage() {
-  const sorted = allPosts
+  const sortedPosts = allPosts
     .filter((p) => p.published)
     .sort(
       (a, b) =>
@@ -20,19 +21,37 @@ export default async function postsPage() {
         new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
     );
 
+  const sortedBooks = allBooks
+    .filter((b) => b.published && b.body.raw.trim().length > 0)
+    .sort(
+      (a, b) =>
+        new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
+        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
+    );
+
+  const allContent = [...sortedPosts, ...sortedBooks].sort(
+    (a, b) =>
+      new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
+      new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
+  );
+
   return (
     <>
       <RegularHeader
-        title="Posts"
+        title="Posts & Book Reviews"
         description="Just sharing my perspective on things"
       />
 
       <StaggeredAnimation staggerDelay={0.3} animationDuration={0.6}>
         <div className="flex flex-col space-y-4 mx-auto lg:mx-0 md:grid-cols-3">
           <div className="grid grid-cols-1 gap-4">
-            {sorted.map((post) => (
-              <Card key={post.slug}>
-                <Article post={post} />
+            {allContent.map((item) => (
+              <Card key={item.slug}>
+                {item.type === "Post" ? (
+                  <Article post={item} />
+                ) : (
+                  <BookArticle book={item} />
+                )}
               </Card>
             ))}
           </div>
